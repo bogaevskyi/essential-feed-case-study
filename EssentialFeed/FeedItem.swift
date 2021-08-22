dd49683bddb1532e34203dd9d6e7fb233d427990
Andrew Bogaevskyi <andreybogaevsky@gmail.com>
2021-08-22 21:54:47 +0300

    Create the FeedItem module

---
+++ EssentialFeed/FeedItem.swift
@@ -0,0 +1,15 @@
+//
+//  FeedItem.swift
+//  EssentialFeed
+//
+//  Created by Andrii Bohaievskyi on 22.08.2021.
+//
+
+import Foundation
+
+struct FeedItem {
+    let id: UUID
+    let description: String?
+    let location: String?
+    let imageURL: URL
+}
