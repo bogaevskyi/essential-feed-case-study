6c0714b45021cb2b9e25d4f495df00d98677c5c1
Andrew Bogaevskyi <andreybogaevsky@gmail.com>
2021-09-11 21:18:22 +0300

    Perform request of RemoteFedLoader tests

---
+++ EssentialFeedTests/RemoteFeedLoaderTests.swift
@@ -0,0 +1,50 @@
+//
+//  RemoteFeedLoaderTests.swift
+//  EssentialFeedTests
+//
+//  Created by Andrii Bohaievskyi on 11.09.2021.
+//
+
+import XCTest
+
+class RemoteFeedLoader {
+    func load() {
+        HTTPClient.shared.get(from: URL(string: "https://a-url.com")!)
+    }
+}
+
+class HTTPClient {
+    static var shared = HTTPClient()
+    
+    func get(from url: URL) {}
+}
+
+class HTTPClientSpy: HTTPClient {
+    
+    var requestedURL: URL?
+    
+    override func get(from url: URL) {
+        requestedURL = url
+    }
+}
+
+class RemoteFeedLoaderTests: XCTestCase {
+
+    func test_init_doesNotRequestDataFromURL() {
+        let client = HTTPClientSpy()
+        HTTPClient.shared = client
+        _ = RemoteFeedLoader()
+        XCTAssertNil(client.requestedURL)
+    }
+    
+    func test_requestDateFromURL() {
+        let client = HTTPClientSpy()
+        HTTPClient.shared = client
+        let sut = RemoteFeedLoader()
+        
+        sut.load()
+        
+        XCTAssertNotNil(client.requestedURL)
+    }
+
+}
