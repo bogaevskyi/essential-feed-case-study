5ffaec1b14cfaeb8ca1745c5aceb500caa9a7e59
Andrew Bogaevskyi <andreybogaevsky@gmail.com>
2022-06-19 14:43:16 +0300

    Chaned code struct

--- EssentialFeedTests/Feed Cache/CodableFeedStoreTests.swift
+++ EssentialFeedTests/Feed Cache/CodableFeedStoreTests.swift
@@ -153,9 +153,9 @@ class CodableFeedStoreTests: XCTestCase {
     }
     
     private func testSpecificStoreURL() -> URL {
-        FileManager.default.urls(
-            for: .cachesDirectory,
-            in: .userDomainMask
-        ).first!.appendingPathComponent("\(type(of: self)).store")
+        FileManager.default
+            .urls(for: .cachesDirectory, in: .userDomainMask)
+            .first!
+            .appendingPathComponent("\(type(of: self)).store")
     }
 }
