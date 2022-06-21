0011e97781079e7ad10a030664f47f30748bb4f0
Andrew Bogaevskyi <andreybogaevsky@gmail.com>
2022-06-21 09:51:29 +0300

    Retrieving from non-empty cache twice delivers same result

--- EssentialFeedTests/Feed Cache/CodableFeedStoreTests.swift
+++ EssentialFeedTests/Feed Cache/CodableFeedStoreTests.swift
@@ -131,12 +131,41 @@ class CodableFeedStoreTests: XCTestCase {
                 case let .found(retrieveFeed, retrieveTimestamp):
                     XCTAssertEqual(retrieveFeed, feed)
                     XCTAssertEqual(retrieveTimestamp, timestamp)
-                    break
                     
                 default:
-                    XCTFail("Expected found result, got \(retrieveResult) instead")
+                    XCTFail("Expected found result with feed \(feed) and timestamp \(timestamp), got \(retrieveResult) instead")
                 }
                 exp.fulfill()
+            }
+        }
+        
+        wait(for: [exp], timeout: 1.0)
+    }
+    
+    func test_retrieveHasNoSideEffectsOnNonEmptyCache() {
+        let sut = makeSUT()
+        let feed = uniqueImageFeed().local
+        let timestamp = Date()
+        let exp = expectation(description: "Wait for cache retrieval")
+        
+        sut.insert(feed, timestamp: timestamp) { insertionError in
+            XCTAssertNil(insertionError, "Extected feed to be inserted successfully")
+            
+            sut.retrieve { firstResult in
+                sut.retrieve { secondResult in
+                    switch (firstResult, secondResult) {
+                    case let (.found(firstFound), .found(secondFound)):
+                        XCTAssertEqual(firstFound.feed, feed)
+                        XCTAssertEqual(firstFound.timestamp, timestamp)
+                        
+                        XCTAssertEqual(secondFound.feed, feed)
+                        XCTAssertEqual(secondFound.timestamp, timestamp)
+                        
+                    default:
+                        XCTFail("Expected retrieve twice from non empty cache to deliver same found result with feed \(feed) and timestamp \(timestamp), got \(firstResult) and \(secondResult) instead")
+                    }
+                    exp.fulfill()
+                }
             }
         }
         
