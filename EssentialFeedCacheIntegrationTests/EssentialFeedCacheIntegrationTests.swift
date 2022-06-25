58efac8e4d86201d29a81311b8325ea3a91c690a
Andrew Bogaevskyi <andreybogaevsky@gmail.com>
2022-06-25 15:44:54 +0300

    Add EssentialFeedCacheIntegrationTests target

---
+++ EssentialFeedCacheIntegrationTests/EssentialFeedCacheIntegrationTests.swift
@@ -0,0 +1,12 @@
+//
+//  EssentialFeedCacheIntegrationTests.swift
+//  EssentialFeedCacheIntegrationTests
+//
+//  Created by Andrew Bogaevskyi on 25.06.2022.
+//
+
+import XCTest
+
+class EssentialFeedCacheIntegrationTests: XCTestCase {
+
+}
