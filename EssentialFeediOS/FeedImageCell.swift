0cd81a0e1b2cae3130325af64e051a932bf2fb4f
Andrew Bogaevskyi <andreybogaevsky@gmail.com>
2022-07-02 21:51:26 +0300

    test_loadFeedCompletion_renderSuccessfullyLoadedFeed

---
+++ EssentialFeediOS/FeedImageCell.swift
@@ -0,0 +1,14 @@
+//
+//  FeedImageCell.swift
+//  EssentialFeediOS
+//
+//  Created by Andrew Bogaevskyi on 02.07.2022.
+//
+
+import UIKit
+
+public final class FeedImageCell: UITableViewCell {
+    public let locationContainer = UIView()
+    public let locationLabel = UILabel()
+    public let descriptionLabel = UILabel()
+}
