221aeb4d539fdeb656cc4b52a4a134da60526d5d
Andrew Bogaevskyi <andreybogaevsky@gmail.com>
2022-07-22 20:19:55 +0300

    Fix localization

--- EssentialFeediOSTests/Feed UI/Helpers/FeedUIIntegrationTests+Localization.swift
+++ EssentialFeediOSTests/Feed UI/Helpers/FeedUIIntegrationTests+Localization.swift
@@ -7,12 +7,12 @@
 
 import Foundation
 import XCTest
-import EssentialFeediOS
+import EssentialFeed
 
 extension FeedUIIntegrationTests {
     func localized(_ key: String, file: StaticString = #file, line: UInt = #line) -> String {
         let tabel = "Feed"
-        let bundle = Bundle(for: FeedViewController.self)
+        let bundle = Bundle(for: FeedPresenter.self)
         let value = bundle.localizedString(forKey: key, value: nil, table: tabel)
         
         if value == key {
