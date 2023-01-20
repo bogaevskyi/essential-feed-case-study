8c985c73f4d55d855a225ab99acdbdc96d5d939e
Andrew Bogaevskyi <andreybogaevsky@gmail.com>
2023-01-20 18:48:14 +0100

    test_sceneWillConnectToSession_configuresRootViewController

---
+++ Essential AppTests/SceneDelegateTests.swift
@@ -0,0 +1,27 @@
+//
+//  SceneDelegateTests.swift
+//  Essential AppTests
+//
+//  Created by Andrew Bogaevskyi on 04.08.2022.
+//
+
+import XCTest
+import EssentialFeediOS
+@testable import Essential_App 
+
+class SceneDelegateTests: XCTestCase {
+
+    func test_sceneWillConnectToSession_configuresRootViewController() {
+        let sut = SceneDelegate()
+        sut.window = UIWindow()
+
+        sut.configureWindow()
+        
+        let root = sut.window?.rootViewController
+        let rootNavigation = root as? UINavigationController
+        let topController = rootNavigation?.topViewController
+        
+        XCTAssertNotNil(rootNavigation)
+        XCTAssertTrue(topController is FeedViewController)
+    }
+}
